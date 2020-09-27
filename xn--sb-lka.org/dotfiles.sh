847df65ad281df3c86f8abd3fc9e78e491759690
Nicolai Søborg <git@xn--sb-lka.org>
2020-09-27 11:15:42 +0200

    dotfiles: add TPM (tmux)

--- xn--sb-lka.org/dotfiles.sh
+++ xn--sb-lka.org/dotfiles.sh
@@ -3,3 +3,4 @@ git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 source ~/.bash_aliases
 dotfiles config --local status.showUntrackedFiles no
 curl -fLso ${XDG_DATA_HOME:-~/.local/share}/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm  # tmux: prefix+I

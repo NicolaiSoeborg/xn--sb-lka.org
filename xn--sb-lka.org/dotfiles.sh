708fadec11aa48f42d68239867b2bd3c2b04c124
Nicolai Søborg <git@xn--sb-lka.org>
2020-11-07 17:07:04 +0100

    Update dotfiles.sh (locale-gen)

--- xn--sb-lka.org/dotfiles.sh
+++ xn--sb-lka.org/dotfiles.sh
@@ -1,5 +1,6 @@
 git clone --bare https://github.com/NicolaiSoeborg/dotfiles.git $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
+locale-gen en_DK.UTF-8
 source ~/.bash_aliases
 dotfiles config --local status.showUntrackedFiles no
 curl -fLso ${XDG_DATA_HOME:-~/.local/share}/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

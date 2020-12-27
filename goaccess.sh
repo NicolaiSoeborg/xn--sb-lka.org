f47ceef4733da73acfb7353fb4e7f0f146fd6f90
Nicolai Søborg <git@xn--sb-lka.org>
2020-12-27 18:57:23 +0100

    Add: goaccess log

---
+++ goaccess.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Credit: https://alexmv12.xyz/blog/goaccess_caddy/
+
+goaccess <(cat /var/log/caddy/access.log | jq --raw-output '
+    .request.remote_addr |= .[:-6] |
+    select(.request.remote_addr != "127.0.0.1") |
+    select(.request.uri | startswith("/_matrix/") | not) |
+    [
+      .common_log,
+      .request.headers.Referer[0] // "-",
+      .request.headers."User-Agent"[0],
+      .duration
+    ] | @csv') --log-format='"%h - - [%d:%t %^] ""%m %r %H"" %s %b","%R","%u",%T' --time-format='%H:%M:%S' --date-format='%d/%b/%Y'
+# -o report.html

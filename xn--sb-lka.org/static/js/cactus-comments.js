e3ca61ae699524012d18af5af34227bf8ffa8a23
Nicolai Søborg <git@xn--sb-lka.org>
2021-05-09 17:38:29 +0200

    Update CactusComment to use data-attributes

--- xn--sb-lka.org/static/js/cactus-comments.js
+++
@@ -1,7 +0,0 @@
-initComments({
-  node: document.getElementById("comment-section"),
-  defaultHomeserverUrl: "https://matrix.cactus.chat:8448",
-  serverName: "cactus.chat",
-  siteName: "søb.org",
-  commentSectionId: "forside"
-})

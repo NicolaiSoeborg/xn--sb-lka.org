982a01c00caefc31ce11aba6c733f6e67015b314
Nicolai Søborg <git@xn--sb-lka.org>
2024-04-27 15:00:44 +0000

    Move server, updates

--- goaccess.sh
+++ goaccess.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-# Credit: https://alexmv12.xyz/blog/goaccess_caddy/
+# (cat /var/log/caddy/access.log ; zcat /var/log/caddy/access-*.log.gz) | jq 'select(.request.uri != "/.well-known/matrix/server")' | goaccess - --log-format=CADDY -o xn--sb-lka.org/rapport.$RANDOM.html
 
-goaccess <(cat /var/log/caddy/access.log | jq --raw-output '
-    .request.remote_addr |= .[:-6] |
-    select(.request.remote_addr != "127.0.0.1") |
-    select(.request.uri | startswith("/_matrix/") | not) |
-    [
-      .common_log,
-      .request.headers.Referer[0] // "-",
-      .request.headers."User-Agent"[0],
-      .duration
-    ] | @csv') --log-format='"%h - - [%d:%t %^] ""%m %r %H"" %s %b","%R","%u",%T' --time-format='%H:%M:%S' --date-format='%d/%b/%Y'
-# --real-time-html -o report.html
+(cat /var/log/caddy/access.log ; zcat /var/log/caddy/access-*.log.gz) | goaccess - --log-format=CADDY -o xn--sb-lka.org/rapport.$RANDOM.html
